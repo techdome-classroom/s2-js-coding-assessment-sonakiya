7f904962e1cbe4a0489483a34a8ce454548486e1
sonakiya <156442702+sonakiya@users.noreply.github.com>
2024-10-07 14:20:20 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -3,9 +3,39 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    
+  // Stack to keep track of opening brackets
+  let stack = [];
+  
+  // Hash map for keeping track of mappings between opening and closing brackets
+  const matchingBrackets = {
+      ')': '(',
+      '}': '{',
+      ']': '['
+  };
+
+  // Iterate through each character in the string
+  for (let char of s) {
+      // If the character is a closing bracket
+      if (char in matchingBrackets) {
+          // Pop the top element from the stack
+          // If the stack is empty, set topElement to a dummy value '#'
+          let topElement = stack.length === 0 ? '#' : stack.pop();
+
+          // If the top element doesn’t match the corresponding opening bracket
+          if (topElement !== matchingBrackets[char]) {
+              return false;
+          }
+      } else {
+          // If it’s an opening bracket, push it to the stack
+          stack.push(char);
+      }
+  }
+
+  // If the stack is empty, then we have matched all brackets correctly
+  return stack.length === 0;
 };
 
 module.exports = { isValid };
+
 
 
